a6591b20a7470996f488b6c570503f5986e3560d
jun-hf <fong.junh@gmail.com>
2022-07-23 18:10:54 +0100

    Creating and adding data to student table

---
+++ student.sql
@@ -0,0 +1,60 @@
+DROP TABLE student;
+
+-- Creating a table
+CREATE TABLE student (
+	student_id INT AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    major VARCHAR(20),
+    PRIMARY KEY(student_id)
+);
+
+-- Describe the data
+
+DESCRIBE student; 
+
+-- Adding and dropping a column
+
+ALTER TABLE student ADD gpa DECIMAL; 
+
+ALTER TABLE student DROP COLUMN gpa;
+
+-- Inserting data
+
+INSERT INTO student(name, major) VALUES('Jack', 'Biology');
+INSERT INTO student(name, major) VALUES('Alice', 'Physics');
+INSERT INTO student(name, major) VALUES('Kate', 'Sociology');
+INSERT INTO student(name, major) VALUES('Claire', 'English');
+INSERT INTO student(name, major) VALUES('Jack', 'Biology');
+INSERT INTO student(name, major) VALUES('Mike', 'Comp. Sci');
+
+-- Updating row
+
+-- Changing all the variable
+UPDATE student
+SET major = 'Bio'
+WHERE major = 'Biology';
+
+UPDATE student
+SET major = 'Computer Science'
+WHERE major = 'Comp. Sci';
+
+UPDATE student
+SET major = 'Biology'
+WHERE major = 'Bio';
+
+-- Changing a specific variable
+UPDATE student
+SET major = 'Computer Science'
+WHERE student_id = 5;
+
+-- Querying 
+
+SELECT * FROM student;
+
+-- Quering the name and major, with a desceding order of the id
+
+SELECT student.name, student.major
+FROM student
+ORDER BY student_id DESC;
+
+
